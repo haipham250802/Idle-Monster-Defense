a1b687ae3c3edfe89f599d1844b1503d6b882ae4
Thu Hà <99801941+Nguyenthithuha2051063641@users.noreply.github.com>
2024-01-13 22:19:05 +0700

    Add files via upload

---
+++ PopUpWin.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
+public class PopUpWin : PopUpBase
+{
+    [SerializeField] private Button claimBonusRWButton;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Text quantityGoldTxt;
+    [SerializeField] private Image iconButton;
+
+    private void Start()
+    {
+        initQuantityGold();
+        initButton();
+    }
+    private void initButton()
+    {
+        claimBonusRWButton.onClick.AddListener(onClickButtonClaimBonusRWButton);
+    }
+    private void onClickButtonNext()
+    {
+        nextButton.onClick.AddListener(onClickButtonNext);
+    }
+    private void onClickButtonClaimBonusRWButton()
+    {
+        claimBonusRWButton.interactable = false;
+        iconButton.color = Color.gray;
+        int currentGold = GamePlayManager.Ins.GetQuantityGold();
+        int newGold = currentGold * 2;
+        DOTween.To(() => currentGold, _ =>
+        {
+            quantityGoldTxt.text = _.ToString();
+        }, newGold, 0.3f);
+    }
+    private void initQuantityGold()
+    {
+        quantityGoldTxt.text = GamePlayManager.Ins.GetQuantityGold().ToString();
+    }
+}
